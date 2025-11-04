8338b6dfced13272401ca4a1d5e48241e4ed42ac
ANKIREDDY SAI SHASHANK REDDY <2410030167@klh.edu.in>
2025-11-04 10:29:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,16 @@ const PORT = process.env.PORT || 5000;
 
 // --- Middleware ---
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://cinereserve-lvu3.onrender.com",
+    "http://localhost:5000",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "token"],
+}));
+
 
 // --- MongoDB Connection ---
 mongoose
